f965247f99f69cddaa57e3775d89135fccc261fd
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-11-16 14:15:48 +0300

    feat: login form

---
+++ src/features/auth/login/ui/login-form.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { Button } from '@/shared/ui/button'
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/shared/ui/card'
+import { Input } from '@/shared/ui/input'
+import { Label } from '@/shared/ui/label'
+</script>
+
+<template>
+  <Card class="w-full max-w-sm">
+    <CardHeader>
+      <CardTitle>Login to your account</CardTitle>
+      <CardDescription> Enter your email below to login to your account</CardDescription>
+      <CardAction>
+        <Button variant="link"> Sign Up</Button>
+      </CardAction>
+    </CardHeader>
+    <CardContent>
+      <form>
+        <div class="grid w-full items-center gap-4">
+          <div class="flex flex-col space-y-1.5">
+            <Label for="email">Email</Label>
+            <Input id="email" type="email" placeholder="m@example.com" />
+          </div>
+          <div class="flex flex-col space-y-1.5">
+            <div class="flex items-center">
+              <Label for="password">Password</Label>
+              <a href="#" class="ml-auto inline-block text-sm underline"> Forgot your password? </a>
+            </div>
+            <Input id="password" type="password" />
+          </div>
+        </div>
+      </form>
+    </CardContent>
+    <CardFooter class="flex flex-col gap-2">
+      <Button variant="outline" class="w-full border-none"> Login</Button>
+      <Button variant="outline" class="w-full border-none"> Login with Google</Button>
+    </CardFooter>
+  </Card>
+</template>
--- src/view/signin/ui/signin-view.vue
+++ src/view/signin/ui/signin-view.vue
@@ -1,74 +1,11 @@
 <script setup lang="ts">
-import SigninImage from '@/assets/images/SigninImage.png'
-import PasswordIcon from '@/assets/icons/password-icon.vue'
-import InputUi from '@/shared/input/input-ui.vue'
-import EmailIcon from '@/assets/icons/email-icon.vue'
-import { useSignIn } from '@/view/signin/model/useSignIn.ts'
-import { useSignInForm } from '@/view/signin/model/useSignInForm.ts'
-import CheckboxUi from '@/shared/checkbox/checkbox-ui.vue'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/shared/ui/dropdown-menu'
-import { Icon } from '@iconify/vue'
-import { useColorMode } from '@vueuse/core'
-import { Button } from '@/shared/ui/button'
-
-const { loginFetch } = useSignIn()
-const { errorMessage, email, password, isRememberMe, handleSubmit } = useSignInForm()
-const onSubmit = () => {
-  handleSubmit(() =>
-    loginFetch({
-      email: email.value,
-      password: password.value,
-      isRememberMe: isRememberMe.value,
-    }),
-  )
-}
-
-// Pass { disableTransition: false } to enable transitions
-const mode = useColorMode()
+import LoginForm from '@/features/auth/login/ui/login-form.vue'
 </script>
 
 <template>
-  <div>
-    <div class="container">
-      <div class="form-container">
-        <div>
-          <DropdownMenu>
-            <DropdownMenuTrigger as-child>
-              <Button variant="outline">
-                <Icon
-                  icon="radix-icons:moon"
-                  class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-                />
-                <Icon
-                  icon="radix-icons:sun"
-                  class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-                />
-                <span class="sr-only">Toggle theme</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuItem @click="mode = 'light'"> Light</DropdownMenuItem>
-              <DropdownMenuItem @click="mode = 'dark'"> Dark</DropdownMenuItem>
-              <DropdownMenuItem @click="mode = 'auto'"> System</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
-        <h1>Sign In</h1>
-
-        <Button type="submit">Login</Button>
-        <div class="links">
-          <span>Don’t have an account? <router-link to="/signup">Create One</router-link></span>
-          <span
-            >Forgot Password? <router-link to="/forgot-password">Reset Password</router-link></span
-          >
-        </div>
-      </div>
-    </div>
+  <div class="flex justify-center items-center flex-col">
+    <h1>Sign In</h1>
+    <LoginForm />
   </div>
 </template>
 
