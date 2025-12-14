28ff1f1dc0d18dfc6b30562ebf869137b0af83ec
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-12-14 17:10:13 +0300

    fix: env

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError, AxiosHeaders, type InternalAxiosRequestConfig } from 'axios'
 
-const baseURL = import.meta.env.API_URL ?? 'http://localhost:4000/api/v1'
+const baseURL = import.meta.env.VITE_API_URL ?? 'http://localhost:4000/api/v1'
 
 export const apiClient = axios.create({
   baseURL,
